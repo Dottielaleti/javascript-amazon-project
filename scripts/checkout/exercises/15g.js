42e625bb63731d2e705788fc92a450d316553f3d
LaletiDottie <laletidottie@gmail.com>
2025-11-28 15:29:23 +0300

    Practicing exercises

---
+++ scripts/checkout/exercises/15g.js
@@ -0,0 +1,5 @@
+ export function isSatSun(date) {
+        const dayOfWeek = date.format('dddd'); 
+          return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+        }
+        export default isSatSun;
